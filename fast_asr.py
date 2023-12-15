48a8ab89bde226563376cae2c48280ed2ca0a5cd
shiiya <52955154+Shiiya0418@users.noreply.github.com>
2023-12-15 13:29:50 +0900

    [fix] fast_asr.py のコード整形
    読みづらいので、多少綺麗にした

--- fast_asr.py
+++ fast_asr.py
@@ -1,4 +1,4 @@
-from transformers import WhisperForConditionalGeneration, WhisperFeatureExtractor,WhisperProcessor
+from transformers import WhisperForConditionalGeneration, WhisperFeatureExtractor, WhisperProcessor
 import torch
 import torchaudio
 from torchaudio.functional import resample
@@ -6,15 +6,15 @@ from torchaudio.functional import resample
 def transcribe(path):
     raw_audio, sr = torchaudio.load(path)
     sr = 16000
-    #raw_audio = resample(waveform=raw_audio,orig_freq=sr,new_freq=16000)
+    #raw_audio = resample(waveform=raw_audio, orig_freq=sr, new_freq=16000)
     raw_audio = raw_audio.squeeze(dim = 0)
-    #feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-tiny")
+    # feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-tiny")
     processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
 
     model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny").cuda()
     input_features = processor(raw_audio, sampling_rate=sr, return_tensors="pt").input_features
     with torch.no_grad():
-        generated_ids = model.generate(input_features.to("cuda"), language="<|ja|>", task = "transcribe")[0]
+        generated_ids = model.generate(input_features.to("cuda"), language="<|ja|>", task="transcribe")[0]
         transcription = processor.decode(generated_ids)
     return processor.tokenizer._normalize(transcription)
 
